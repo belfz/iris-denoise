06bac7bc5160c5e1468283b044ee26f841ce9bd7
Marcin Baraniecki <marcin.baraniecki@softwaremill.com>
2025-12-29 23:04:28 +0100

    base tests

--- src/main.rs
+++ src/main.rs
@@ -48,14 +48,14 @@ struct Args {
 }
 
 fn main() {
-    if let Err(err) = run() {
+    let args = Args::parse();
+    if let Err(err) = run(args) {
         eprintln!("error: {err}");
         process::exit(1);
     }
 }
 
-fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::parse();
+fn run(args: Args) -> Result<(), Box<dyn std::error::Error>> {
     let output_path =
         args.output
             .unwrap_or_else(|| default_output_path(&args.input, args.strength, args.sharpen, args.experimental));
@@ -79,6 +79,7 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
     );
     Ok(())
 }
+
 fn default_output_path(input: &Path, strength: u8, sharpen: Option<u8>, experimental: bool) -> PathBuf {
     let parent = input.parent().unwrap_or_else(|| Path::new("."));
     let stem = input
@@ -91,4 +92,88 @@ fn default_output_path(input: &Path, strength: u8, sharpen: Option<u8>, experime
     parent.join(format!(
         "{stem}_denoised_strength-{strength}{experimental_suffix}{sharpen_suffix}.png"
     ))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use image::{ImageBuffer, Rgb, RgbImage};
+    use std::fs;
+    use std::time::{SystemTime, UNIX_EPOCH};
+
+    fn temp_dir(prefix: &str) -> PathBuf {
+        let mut dir = std::env::temp_dir();
+        let nanos = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_nanos();
+        dir.push(format!("{prefix}_{}_{}", std::process::id(), nanos));
+        fs::create_dir_all(&dir).expect("create temp dir");
+        dir
+    }
+
+    fn write_test_image(path: &Path) {
+        let mut img: RgbImage = ImageBuffer::new(3, 2);
+        // Row 0
+        img.put_pixel(0, 0, Rgb([10, 20, 30]));
+        img.put_pixel(1, 0, Rgb([40, 50, 60]));
+        img.put_pixel(2, 0, Rgb([90, 80, 70]));
+        // Row 1 with a bright "star"
+        img.put_pixel(0, 1, Rgb([200, 200, 200]));
+        img.put_pixel(1, 1, Rgb([30, 10, 5]));
+        img.put_pixel(2, 1, Rgb([5, 10, 30]));
+        img.save(path).expect("save test image");
+    }
+
+    fn load_rgb(path: &Path) -> RgbImage {
+        image::open(path).expect("open image").to_rgb8()
+    }
+
+    #[test]
+    fn run_standard_denoise_no_sharpen_matches_expected() {
+        let dir = temp_dir("denoise_std");
+        let input = dir.join("input.png");
+        let output = dir.join("out.png");
+        write_test_image(&input);
+
+        let args = Args {
+            input: input.clone(),
+            output: Some(output.clone()),
+            strength: 3,
+            sharpen: None,
+            experimental: false,
+        };
+
+        run(args).expect("run standard");
+
+        let produced = load_rgb(&output);
+        let expected = denoise_image(load_image(&input).unwrap(), 3).to_rgb8();
+        assert_eq!(produced.as_raw(), expected.as_raw());
+    }
+
+    #[test]
+    fn run_experimental_with_luma_sharpen_matches_expected() {
+        let dir = temp_dir("denoise_exp");
+        let input = dir.join("input.png");
+        let output = dir.join("out.png");
+        write_test_image(&input);
+
+        let args = Args {
+            input: input.clone(),
+            output: Some(output.clone()),
+            strength: 3,
+            sharpen: Some(3),
+            experimental: true,
+        };
+
+        run(args).expect("run experimental");
+
+        let produced = load_rgb(&output);
+        let expected = sharpen_image_luma(
+            denoise_image_experimental(load_image(&input).unwrap(), 3),
+            3,
+        )
+        .to_rgb8();
+        assert_eq!(produced.as_raw(), expected.as_raw());
+    }
 }
